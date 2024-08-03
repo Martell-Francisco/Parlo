968454f2d0861ffeda8a893491bb08ab14da2c49
MARTELL-FRANCISCO <franciscomartell@att.net>
2024-08-03 12:11:16 -0500

    deploy test

--- parlo/app/protected/page.tsx
+++ parlo/app/protected/page.tsx
@@ -27,7 +27,7 @@ const groupid = {}
       <div className="w-full">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            {/* @ts-expect-error Server Component */}
+           
             <AuthButton />
           </div>
         </nav>
